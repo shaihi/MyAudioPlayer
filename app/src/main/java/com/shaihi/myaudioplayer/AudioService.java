5673e8e7d20a3bf4e6c75bdd938213d45113aaa7
shaihi <shaihi@icloud.com>
2025-09-30 15:06:45 +0300

    Added service and add the override functions that are needed

---
+++ app/src/main/java/com/shaihi/myaudioplayer/AudioService.java
@@ -0,0 +1,31 @@
+package com.shaihi.myaudioplayer;
+
+import android.app.Service;
+import android.content.Intent;
+import android.os.IBinder;
+
+public class AudioService extends Service {
+    public AudioService() {
+    }
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+    }
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+    }
+
+    @Override
+    public int onStartCommand(Intent intent, int flags, int startId) {
+        return super.onStartCommand(intent, flags, startId);
+    }
+
+    @Override
+    public IBinder onBind(Intent intent) {
+        // TODO: Return the communication channel to the service.
+        throw new UnsupportedOperationException("Not yet implemented");
+    }
+}
